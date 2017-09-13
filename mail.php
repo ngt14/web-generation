6493824450febe9aa9d16b16121e4f97bcb7e200
pierre <pierre.thiebert@outlook.fr>
2017-09-13 17:53:08 +0200

    Ajout code source

---
+++ mail.php
@@ -0,0 +1,13 @@
+<?php
+	/*$msg = 'demandeur : '.$_POST["name"].' / adresse mail : '.$_POST["email"].' / msg : '.$_POST["msg"];
+	$to = "pierre.thiebert@outlook.fr";
+	$subject = "Demande de devis";
+
+	$message=$msg;
+
+	$headers  = 'MIME-Version: 1.0' . "\r\n";
+	$headers .= 'Content-type: text/html; charset=UTF-8' . "\r\n";
+
+	mail($to, $subject, $message, $headers);*/
+	header('location:felicitation.php');
+?> 
---
+++ refonte.php
@@ -0,0 +1,53 @@
+<html>
+  <head>
+    <title>Web-G&eacute;n&eacute;ration</title>
+    <link rel="stylesheet" type="text/css" href="style.css" />
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<script type="text/javascript" src="script.js"></script>
+  </head>
+  <body>
+	<?php include 'header.php';?>
+  <section id="bestof">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-8 col-sm-offset-2">
+          </div>
+        </div>
+      </div>
+    </section>
+	 <section id="omeni">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-8 col-md-6 animated slideInLeft">
+            <div class="row">
+              <div class="col-lg-8">
+                <h2>Refonte de site existant</h2>
+              </div><!--col-->
+            </div><!--row-->
+            <h4>
+              Vous avez d&eacute;j&agrave; votre site mais il est vieillissant ? Vous voulez lui donner un coups de jeune ? C'est le moment du lifting, ensemble donnons &agrave; votre site une seconde jeunesse. Attirez une nouvelle client&egrave;le grace &agrave; un site moderne et dynamique.
+            </h4>
+			<h4>
+              &Agrave; partir de 500€, offrez vous un site moderne et au gout du jour.
+            </h4>
+            <h4>
+              <a href="details.php?choix=r"><button type="button" class="btn btn-secondary">Faire un devis</button></a>
+            </h4>
+          </div><!--col-->
+        </div><!--row-->
+      </div><!--container-->
+    </section>
+	  <section id="bestof">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-8 col-md-6 animated slideInLeft" style="height : 40%;">
+			&nbsp;
+          </div>
+        </div>
+      </div>
+    </section>
+  </body>
+</html>
